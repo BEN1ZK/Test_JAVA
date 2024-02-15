5c5551842c87b3de6e216a17a088d88a6dbbdc23
BEN1ZK <134963857+BEN1ZK@users.noreply.github.com>
2024-02-15 17:19:32 +0200

    CurrencyConverter.java

--- Test/src/edu/step/conversion/CurrencyConverter.java
+++ Test/src/edu/step/conversion/CurrencyConverter.java
@@ -28,19 +28,13 @@ public class CurrencyConverter {
     }
 
     public static void main(String[] args) {
-        CurrencyConverter converter = new CurrencyConverter(19.8); // exemplu de rata de schimb initiala
+        CurrencyConverter converter = new CurrencyConverter(19.8); 
         double amountInRON = 100;
         double amountInEUR = 50;
-
-        // Testare conversie RON -> EUR
         double resultInEUR = converter.convertToEUR(amountInRON);
         System.out.println(amountInRON + " RON = " + resultInEUR + " EUR");
-
-        // Testare conversie EUR -> RON
         double resultInRON = converter.convertToRON(amountInEUR);
         System.out.println(amountInEUR + " EUR = " + resultInRON + " RON");
-
-        // Setare rata de schimb noua
-        converter.setExchangeRate(20.0); // exemplu de noua rata de schimb
+        converter.setExchangeRate(20.0); 
     }
 }
